c5506b0d2aa85944c1abc5ef1d65ef3401b6c7e2
arch_rocko <andres@fontalvo.xyz>
2025-07-29 11:53:19 -0500

    feat: add health check base

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
+
 
 
 func main(){
+	const filepathRoot = "."
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle(
+		"/app/",
+		http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))),
+	)
+	mux.HandleFunc("/healthz", handlerHealthCheck)
 	server := http.Server{
 		Addr: ":8080",
 		Handler: mux,
 	}
 
-	server.ListenAndServe()
+	log.Fatal(server.ListenAndServe())
 }
+
+
+func handlerHealthCheck(w http.ResponseWriter, r *http.Request){
+			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+
